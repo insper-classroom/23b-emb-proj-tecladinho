d14e8e46bc399b3f946761cca506d0b0af961f3f
rodme02 <rodrigomedeiros2002@gmail.com>
2023-10-30 15:31:58 -0300

    Update main.c

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,17 +1,20 @@
-/************************************************************************
-* 5 semestre - Eng. da Computao - Insper
-*
-* 2021 - Exemplo com HC05 com RTOS
-*
-*/
-
 #include <asf.h>
-#include "conf_board.h"
 #include <string.h>
 
-/************************************************************************/
-/* defines                                                              */
-/************************************************************************/
+#include "conf_board.h"
+#include "gfx_mono_ug_2832hsweg04.h"
+#include "gfx_mono_text.h"
+#include "sysfont.h"
+
+//#define DEBUG_SERIAL
+
+#ifdef DEBUG_SERIAL
+#define USART_COM USART1
+#define USART_COM_ID ID_USART1
+#else
+#define USART_COM USART0
+#define USART_COM_ID ID_USART0
+#endif
 
 // LEDs
 #define LED_PIO      PIOC
@@ -25,91 +28,39 @@
 #define BUT_IDX      11
 #define BUT_IDX_MASK (1 << BUT_IDX)
 
-// PA19 - C
-// PC31 - C#
-// PB2 - D
-// PB3 - D#
-// PC30 - E
-// PA0 - F
-// PC17 - F#
-// PD28 - G
-// PA4 - G#
-// PA3 - A
-// PB1 - A#
-// PB0 - B
 
-
-// usart (bluetooth ou serial)
-// Descomente para enviar dados
-// pela serial debug
-
- //#define DEBUG_SERIAL
-
-#ifdef DEBUG_SERIAL
-#define USART_COM USART1
-#define USART_COM_ID ID_USART1
-#else
-#define USART_COM USART0
-#define USART_COM_ID ID_USART0
-#endif
-
-/************************************************************************/
-/* RTOS                                                                 */
-/************************************************************************/
+/** RTOS  */
+#define TASK_OLED_STACK_SIZE                (1024*6/sizeof(portSTACK_TYPE))
+#define TASK_OLED_STACK_PRIORITY            (tskIDLE_PRIORITY)
 
 #define TASK_BLUETOOTH_STACK_SIZE            (4096/sizeof(portSTACK_TYPE))
 #define TASK_BLUETOOTH_STACK_PRIORITY        (tskIDLE_PRIORITY)
 
-/************************************************************************/
-/* prototypes                                                           */
-/************************************************************************/
-
-extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
-signed char *pcTaskName);
+extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,  signed char *pcTaskName);
 extern void vApplicationIdleHook(void);
 extern void vApplicationTickHook(void);
 extern void vApplicationMallocFailedHook(void);
 extern void xPortSysTickHandler(void);
 
-/************************************************************************/
-/* constants                                                            */
-/************************************************************************/
+/** prototypes */
+void but_callback(void);
+static void BUT_init(void);
+void io_init(void);
 
 /************************************************************************/
-/* variaveis globais                                                    */
+/* RTOS application funcs                                               */
 /************************************************************************/
 
-/************************************************************************/
-/* RTOS application HOOK                                                */
-/************************************************************************/
-
-/* Called if stack overflow during execution */
-extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
-signed char *pcTaskName) {
+extern void vApplicationStackOverflowHook(xTaskHandle *pxTask, signed char *pcTaskName) {
 	printf("stack overflow %x %s\r\n", pxTask, (portCHAR *)pcTaskName);
-	/* If the parameters have been corrupted then inspect pxCurrentTCB to
-	* identify which task has overflowed its stack.
-	*/
-	for (;;) {
-	}
+	for (;;) {	}
 }
 
-/* This function is called by FreeRTOS idle task */
-extern void vApplicationIdleHook(void) {
-	pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
-}
+extern void vApplicationIdleHook(void) { }
 
-/* This function is called by FreeRTOS each tick */
 extern void vApplicationTickHook(void) { }
 
 extern void vApplicationMallocFailedHook(void) {
-	/* Called if a call to pvPortMalloc() fails because there is insufficient
-	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
-	internally by FreeRTOS API functions that create tasks, queues, software
-	timers, and semaphores.  The size of the FreeRTOS heap is set by the
-	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
-
-	/* Force an assert. */
 	configASSERT( ( volatile void * ) NULL );
 }
 
@@ -117,164 +68,177 @@ extern void vApplicationMallocFailedHook(void) {
 /* handlers / callbacks                                                 */
 /************************************************************************/
 
+void but_callback(void) {
+  
+}
+
 /************************************************************************/
 /* funcoes                                                              */
 /************************************************************************/
 
-void io_init(void) {
-
-	// Ativa PIOs
-	pmc_enable_periph_clk(LED_PIO_ID);
-	pmc_enable_periph_clk(BUT_PIO_ID);
-
-	// Configura Pinos
-	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT | PIO_DEBOUNCE);
-	pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP);
-}
-
 static void configure_console(void) {
 	const usart_serial_options_t uart_serial_options = {
 		.baudrate = CONF_UART_BAUDRATE,
-		#if (defined CONF_UART_CHAR_LENGTH)
 		.charlength = CONF_UART_CHAR_LENGTH,
-		#endif
 		.paritytype = CONF_UART_PARITY,
-		#if (defined CONF_UART_STOP_BITS)
 		.stopbits = CONF_UART_STOP_BITS,
-		#endif
 	};
 
 	/* Configure console UART. */
 	stdio_serial_init(CONF_UART, &uart_serial_options);
 
 	/* Specify that stdout should not be buffered. */
-	#if defined(__GNUC__)
 	setbuf(stdout, NULL);
-	#else
-	/* Already the case in IAR's Normal DLIB default configuration: printf()
-	* emits one character at a time.
-	*/
-	#endif
+}
+
+void io_init(void) {
+
+  // Ativa PIOs
+  pmc_enable_periph_clk(LED_PIO_ID);
+  pmc_enable_periph_clk(BUT_PIO_ID);
+
+  // Configura Pinos
+  pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT | PIO_DEBOUNCE);
+  pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP);
 }
 
 uint32_t usart_puts(uint8_t *pstring) {
-	uint32_t i ;
+  uint32_t i ;
 
-	while(*(pstring + i))
-	if(uart_is_tx_empty(USART_COM))
-	usart_serial_putchar(USART_COM, *(pstring+i++));
+  while(*(pstring + i))
+  if(uart_is_tx_empty(USART_COM))
+  usart_serial_putchar(USART_COM, *(pstring+i++));
 }
 
 void usart_put_string(Usart *usart, char str[]) {
-	usart_serial_write_packet(usart, str, strlen(str));
+  usart_serial_write_packet(usart, str, strlen(str));
 }
 
 int usart_get_string(Usart *usart, char buffer[], int bufferlen, uint timeout_ms) {
-	uint timecounter = timeout_ms;
-	uint32_t rx;
-	uint32_t counter = 0;
+  uint timecounter = timeout_ms;
+  uint32_t rx;
+  uint32_t counter = 0;
 
-	while( (timecounter > 0) && (counter < bufferlen - 1)) {
-		if(usart_read(usart, &rx) == 0) {
-			buffer[counter++] = rx;
-		}
-		else{
-			timecounter--;
-			vTaskDelay(1);
-		}
-	}
-	buffer[counter] = 0x00;
-	return counter;
+  while( (timecounter > 0) && (counter < bufferlen - 1)) {
+    if(usart_read(usart, &rx) == 0) {
+      buffer[counter++] = rx;
+    }
+    else{
+      timecounter--;
+      vTaskDelay(1);
+    }
+  }
+  buffer[counter] = 0x00;
+  return counter;
 }
 
 void usart_send_command(Usart *usart, char buffer_rx[], int bufferlen,
 char buffer_tx[], int timeout) {
-	usart_put_string(usart, buffer_tx);
-	usart_get_string(usart, buffer_rx, bufferlen, timeout);
+  usart_put_string(usart, buffer_tx);
+  usart_get_string(usart, buffer_rx, bufferlen, timeout);
 }
 
-void config_usart1(void) {
-	sysclk_enable_peripheral_clock(ID_USART1);
-	usart_serial_options_t config;
-	config.baudrate = 9600;
-	config.charlength = US_MR_CHRL_8_BIT;
-	config.paritytype = US_MR_PAR_NO;
-	config.stopbits = false;
-	usart_serial_init(USART1, &config);
-	usart_enable_tx(USART1);
-	usart_enable_rx(USART1);
+void config_usart0(void) {
+  sysclk_enable_peripheral_clock(ID_USART0);
+  usart_serial_options_t config;
+  config.baudrate = 9600;
+  config.charlength = US_MR_CHRL_8_BIT;
+  config.paritytype = US_MR_PAR_NO;
+  config.stopbits = false;
+  usart_serial_init(USART0, &config);
+  usart_enable_tx(USART0);
+  usart_enable_rx(USART0);
 
-	// RX - PA21 A  TX - PB4 D
-	pio_configure(PIOA, PIO_PERIPH_A, (1 << 21), PIO_DEFAULT);
-	pio_configure(PIOB, PIO_PERIPH_D, (1 << 4), PIO_DEFAULT);
+  // RX - PB0  TX - PB1
+  pio_configure(PIOB, PIO_PERIPH_C, (1 << 0), PIO_DEFAULT);
+  pio_configure(PIOB, PIO_PERIPH_C, (1 << 1), PIO_DEFAULT);
 }
 
 int hc05_init(void) {
-	char buffer_rx[128];
-	usart_send_command(USART_COM, buffer_rx, 1000, "AT", 100);
-	vTaskDelay( 500 / portTICK_PERIOD_MS);
-	usart_send_command(USART_COM, buffer_rx, 1000, "AT", 100);
-	vTaskDelay( 500 / portTICK_PERIOD_MS);
-	usart_send_command(USART_COM, buffer_rx, 1000, "AT+NAMERodrigo", 100);
-	vTaskDelay( 500 / portTICK_PERIOD_MS);
-	usart_send_command(USART_COM, buffer_rx, 1000, "AT", 100);
-	vTaskDelay( 500 / portTICK_PERIOD_MS);
-	usart_send_command(USART_COM, buffer_rx, 1000, "AT+PIN0000", 100);
+  char buffer_rx[128];
+  usart_send_command(USART_COM, buffer_rx, 1000, "AT", 100);
+  vTaskDelay( 500 / portTICK_PERIOD_MS);
+  usart_send_command(USART_COM, buffer_rx, 1000, "AT", 100);
+  vTaskDelay( 500 / portTICK_PERIOD_MS);
+  usart_send_command(USART_COM, buffer_rx, 1000, "AT+NAMEATecladinho", 100);
+  vTaskDelay( 500 / portTICK_PERIOD_MS);
+  usart_send_command(USART_COM, buffer_rx, 1000, "AT", 100);
+  vTaskDelay( 500 / portTICK_PERIOD_MS);
+  usart_send_command(USART_COM, buffer_rx, 1000, "AT+PIN1234", 100);
+  vTaskDelay( 500 / portTICK_PERIOD_MS);
 }
 
 /************************************************************************/
 /* TASKS                                                                */
 /************************************************************************/
 
+static void task_oled(void *pvParameters) {
+  gfx_mono_ssd1306_init();
+  gfx_mono_draw_string("Exemplo RTOS", 0, 0, &sysfont);
+  gfx_mono_draw_string("a", 0, 20, &sysfont);
+
+  for (;;)  {
+  }
+}
+
 void task_bluetooth(void) {
-	printf("Task Bluetooth started \n");
-	
-	printf("Inicializando HC05 \n");
-	config_usart1();
-	hc05_init();
+  printf("Task Bluetooth started \n");
+  
+  printf("Inicializando HC05 \n");
+  #ifndef DEBUG_SERIAL
+  config_usart0();
+  #endif
+  hc05_init();
 
-	// configura LEDs e Botões
-	io_init();
+  // configura LEDs e Botões
+  io_init();
 
-	char button1 = '0';
-	char eof = 'X';
+  char button1 = '0';
+  char eof = 'X';
 
-	// Task não deve retornar.
-	while(1) {
-		// atualiza valor do botão
-		if(pio_get(BUT_PIO, PIO_INPUT, BUT_IDX_MASK) == 0) {
-			button1 = '1';
-		} else {
-			button1 = '0';
-		}
+  // Task não deve retornar.
+  while(1) {
+    // atualiza valor do botão
+    if(pio_get(BUT_PIO, PIO_INPUT, BUT_IDX_MASK) == 0) {
+      button1 = '1';
+      } else {
+      button1 = '0';
+    }
 
-		// envia status botão
-		while(!usart_is_tx_ready(USART_COM)) {
-			vTaskDelay(10 / portTICK_PERIOD_MS);
-		}
-		usart_write(USART_COM, button1);
-		
-		// envia fim de pacote
-		while(!usart_is_tx_ready(USART_COM)) {
-			vTaskDelay(10 / portTICK_PERIOD_MS);
-		}
-		usart_write(USART_COM, eof);
+    // envia status botão
+    while(!usart_is_tx_ready(USART_COM)) {
+      vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+    usart_write(USART_COM, button1);
+    
+    // envia fim de pacote
+    while(!usart_is_tx_ready(USART_COM)) {
+      vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+    usart_write(USART_COM, eof);
 
-		// dorme por 500 ms
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-	}
+    // dorme por 500 ms
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
 }
 
 /************************************************************************/
 /* main                                                                 */
 /************************************************************************/
 
+
 int main(void) {
 	/* Initialize the SAM system */
 	sysclk_init();
 	board_init();
 
+	/* Initialize the console uart */
 	configure_console();
+
+	/* Create task to control oled */
+	if (xTaskCreate(task_oled, "oled", TASK_OLED_STACK_SIZE, NULL, TASK_OLED_STACK_PRIORITY, NULL) != pdPASS) {
+	  printf("Failed to create oled task\r\n");
+	}
 
 	/* Create task to make led blink */
 	xTaskCreate(task_bluetooth, "BLT", TASK_BLUETOOTH_STACK_SIZE, NULL,	TASK_BLUETOOTH_STACK_PRIORITY, NULL);
@@ -282,6 +246,7 @@ int main(void) {
 	/* Start the scheduler. */
 	vTaskStartScheduler();
 
+  /* RTOS não deve chegar aqui !! */
 	while(1){}
 
 	/* Will only get here if there was insufficient memory to create the idle task. */
